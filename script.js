3a433997eb77146b85921c82fd3405345cb0a20a
RahulBurugoju <rahul9399555@gmail.com>
2026-05-16 10:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let hEle = document.querySelector('#display');
+
+function getWindowSize(){
+	let width = window.innerWidth;
+	 const height = window.innerHeight;
+	hEle.textContent = `Width: ${width} and Height: ${height}`
+}
+getWindowSize();
+window.addEventListener('resize',getWindowSize);
